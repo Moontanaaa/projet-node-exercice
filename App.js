09aa860a9d4215bdb81dc1a3c35822968af02864
Moontanaaa <montanafabio@icloud.com>
2025-01-07 15:41:49 +0100

    nodeJs +

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import express from 'express'
+import express from 'express';
+import { musics } from './data/musics.js';
+import dotenv from 'dotenv';
 
-const App = express()
+dotenv.config();
 
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-app.get('/', (request, response) => {
-    response.send(`Welcome to my API`)
-})
+app.get('/', (req, res) => {
+    res.send(`Welcome to my API`);
+});
 
+app.get('/musics', (req, res) => {
+    res.json(musics);
+});
 
+app.get('/musics/:id', (req, res) => {
+    let { id } = req.params
+    try {
+        const musicById = musics.find(music => music.id === parseInt(id))
+        if (!musicById) {
+            return res.status(403).json({message : "music not found"})
+        }
+        return res.status(200).json(musicById)
+    }
+    catch(err){
+        return res.status(500).json({message : "internal server error"})
+    }
+});
 
-
-
-App.listen(8000, () => console.log('server is running'))
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
---
+++ data/musics.js
@@ -0,0 +1,63 @@
+export const musics = [
+    {
+      id: 1,
+      genre: "Techno",
+      auteur: "Jaydee",
+      nom: "Plastic Dreams"
+    },
+    {
+      id: 2,
+      genre: "Techno",
+      auteur: "Joey Beltram",
+      nom: "Energy Flash"
+    },
+    {
+      id: 3,
+      genre: "Techno",
+      auteur: "Derrick May",
+      nom: "Strings of Life"
+    },
+    {
+      id: 4,
+      genre: "Techno",
+      auteur: "Jeff Mills",
+      nom: "The Bells"
+    },
+    {
+      id: 5,
+      genre: "Techno",
+      auteur: "Carl Cox",
+      nom: "Acid Charge"
+    },
+    {
+      id: 6,
+      genre: "Techno",
+      auteur: "Laurent Garnier",
+      nom: "Crispy Bacon"
+    },
+    {
+      id: 7,
+      genre: "Techno",
+      auteur: "Charlotte de Witte",
+      nom: "Selected"
+    },
+    {
+      id: 8,
+      genre: "Techno",
+      auteur: "Amelie Lens",
+      nom: "Stay With Me"
+    },
+    {
+      id: 9,
+      genre: "Techno",
+      auteur: "Adam Beyer",
+      nom: "Your Mind"
+    },
+    {
+      id: 10,
+      genre: "Techno",
+      auteur: "Nina Kraviz",
+      nom: "Ghetto Kraviz"
+    }
+  ];
+  
