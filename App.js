534c36626aa7c46c8d457476c1c50cf9dc59b423
Moontanaaa <montanafabio@icloud.com>
2025-01-07 16:24:00 +0100

    Update App.js

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: false }))
 
 
 app.get('/', (req, res) => {
@@ -24,20 +24,20 @@ app.get('/musics/:id', (req, res) => {
     try {
         const musicById = musics.find(music => music.id === parseInt(id))
         if (!musicById) {
-            return res.status(403).json({message : "music not found"})
+            return res.status(403).json({ message: "music not found" })
         }
         return res.status(200).json(musicById)
     }
-    catch(err){
-        return res.status(500).json({message : "internal server error"})
+    catch (err) {
+        return res.status(500).json({ message: "internal server error" })
     }
 });
 
-app.post('/musics', (req,res) => {
-    let {name,author,genre} = req.body
-    try{
-        if(!name || !author || !genre) {
-            return res.status(401).json({message : "required for all"})
+app.post('/musics', (req, res) => {
+    let { name, author, genre } = req.body
+    try {
+        if (!name || !author || !genre) {
+            return res.status(401).json({ message: "required for all" })
         }
         const newMusic = {
             id: musics.length + 1,
@@ -48,9 +48,37 @@ app.post('/musics', (req,res) => {
         musics.push(newMusic)
         return res.status(201).json(musics)
     }
-    catch(err){
-        return res.status(500).json({message : "Internal server error"})
+    catch (err) {
+        return res.status(500).json({ message: "Internal server error" })
     }
 })
+
+app.put('/musics/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, author, genre } = req.body;
+
+    try {
+        const musicIndex = musics.findIndex(music => music.id === parseInt(id));
+
+        if (musicIndex === -1) {
+            return res.status(404).json({ message: "not find" });
+        }
+
+        if (!name || !author || !genre) {
+            return res.status(400).json({ message: "Required" });
+        }
+
+        musics[musicIndex] = {
+            id: musics[musicIndex].id,
+            name,
+            author,
+            genre,
+        };
+
+        return res.status(200).json(musics[musicIndex]);
+    } catch (err) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+});
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
